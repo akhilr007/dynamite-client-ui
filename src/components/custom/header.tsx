86923ccdeb7239a17fb971fafed1a73138845d58
Akhil Rajan <rajanakhil07@gmail.com>
2024-10-05 20:20:26 +0530

    add navbar

--- src/components/custom/header.tsx
+++ src/components/custom/header.tsx
@@ -6,11 +6,14 @@ import {
     SelectTrigger,
     SelectValue
 } from "@/components/ui/select";
+import Link from "next/link";
+import { Phone, ShoppingBasket } from "lucide-react";
+import { Button } from "../ui/button";
 
 export const Header = () => {
     return (
         <header className="bg-white">
-            <nav className="container py-5 mx-auto">
+            <nav className="container py-5 flex mx-auto items-center justify-between">
                 <div className="flex items-center space-x-4">
                     <svg
                         width="93"
@@ -43,6 +46,35 @@ export const Header = () => {
                             </SelectItem>
                         </SelectContent>
                     </Select>
+                </div>
+                <div className="flex items-center gap-x-4">
+                    <ul className="flex items-center font-medium space-x-4">
+                        <li>
+                            <Link className="hover:text-primary" href={"/"}>
+                                Menu
+                            </Link>
+                        </li>
+                        <li>
+                            <Link className="hover:text-primary" href={"/"}>
+                                Orders
+                            </Link>
+                        </li>
+                    </ul>
+                    <div className="relative">
+                        <Link className="hover:text-primary" href={"/cart"}>
+                            <ShoppingBasket />
+                        </Link>
+                        <span className="absolute -top-4 -right-4 h-6 w-6 flex items-center justify-center rounded-full bg-primary font-semibold text-white">
+                            5
+                        </span>
+                    </div>
+                    <div className="flex items-center ml-12">
+                        <Phone />
+                        <span>+1 9999999999</span>
+                    </div>
+                    <div>
+                        <Button size="sm">Logout</Button>
+                    </div>
                 </div>
             </nav>
         </header>
