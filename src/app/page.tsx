60677c20fb1609bf2f997d87b21f0b801f6c6b9b
Akhil Rajan <rajanakhil07@gmail.com>
2024-10-05 14:24:03 +0530

    add shadcn ui theme

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,10 @@
+import { Button } from "@/components/ui/button";
+
 export default function Home() {
-    return <div className="">Namaste Everyone</div>;
+    return (
+        <div className="">
+            Namaste Everyone
+            <Button>Click Me!</Button>
+        </div>
+    );
 }
