f330f7df65e6d9cb5ee01b7123ab55ae6dc7b125
Akhil Rajan <rajanakhil07@gmail.com>
2024-10-05 18:26:13 +0530

    add logo and select box

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -3,7 +3,7 @@ import { Button } from "@/components/ui/button";
 export default function Home() {
     return (
         <div className="">
-            Namaste Everyone
+            <h3>Pizza Shop</h3>
             <Button>Click Me!</Button>
         </div>
     );
